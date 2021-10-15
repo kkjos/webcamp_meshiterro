93ff49dbf052bd7e006f1c80ecf15157b4e53e40
kkjos <k19951227@gmail.com>
2021-10-15 02:58:02 +0000

    [add]ユーザー認証・画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9208e2640f911c965a6e9fc6af5d39112f43b5234aafc4af5593db0788183fc85a9093f6d71af19ae9f2340549e95dc1d802113160079157d557689b8edc9fd5'
